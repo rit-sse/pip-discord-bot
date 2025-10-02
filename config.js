e9a9919ce47f78699ac3362781cc9262c5ff1e91
pawnyy <6138209+pawnyy@users.noreply.github.com>
2025-10-01 23:51:26 -0400

    update config to use new .env vars

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-
+const crypto = require('node:crypto');
 // Creates the client for the bot with the proper 'intents' it may need.
 // Will be set up with process.env or otherwise, not saved directly within the GitHub.
 // A bot is not connected yet.
@@ -8,7 +8,11 @@ module.exports = {
     clientId: process.env.TEST_CLIENT_ID, // The ID of the bot
     guildId: process.env.TEST_SERVER_ID, // The ID of the server (currently set to a test server)
     name: 'Pip',
-    TOKEN: process.env.DISCORD_TOKEN // Must be kept secret! Uploading this publicly will automatically reset it.
+    TOKEN: process.env.DISCORD_TOKEN, // Must be kept secret! Uploading this publicly will automatically reset it.
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    SIGNING_SECRET: crypto.randomUUID(), // FIXME: Make a permanent secret in production
+    REDIRECT_URI: process.env.REDIRECT_URI
 }
 
 //  Within the slash_commands folder, the "config" parameter references this file and can get these properties (e.g "config.client").
