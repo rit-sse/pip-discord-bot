89704c754da272ccd5037b0cfc21f1a50c783cec
Orion <orion.mulgrew@gmail.com>
2025-10-08 22:08:11 -0400

    Add: DB schema

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+BEGIN;
+
+-- Table: users
+CREATE TABLE IF NOT EXISTS users (
+    discord_id BIGINT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    is_banned BOOLEAN NOT NULL,
+    ban_reason VARCHAR(255),
+    verified_at TIMESTAMP WITH TIME ZONE,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+-- Function to automate updated_at field
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+   NEW.updated_at = NOW();
+   RETURN NEW;
+END;
+$$ language 'plpgsql';
+
+-- Apply the trigger to the tables that have an 'updated_at' column.
+CREATE TRIGGER update_users_updated_at
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE FUNCTION update_updated_at_column();
+
+COMMIT;
